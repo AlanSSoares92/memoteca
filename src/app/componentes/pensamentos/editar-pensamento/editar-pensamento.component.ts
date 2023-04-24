dc126178e05d371b28437e737b05bf9ebf17fdc4
AlanSSoares92 <alan_s_soares@outlook.com>
2023-04-23 22:02:23 -0300

    fim

--- src/app/componentes/pensamentos/editar-pensamento/editar-pensamento.component.ts
+++ src/app/componentes/pensamentos/editar-pensamento/editar-pensamento.component.ts
@@ -1,3 +1,6 @@
+import { ActivatedRoute, Router } from '@angular/router';
+import { PensamentoService } from '../pensamento.service';
+import { Pensamento } from './../pensamento/pensamento';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +9,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./editar-pensamento.component.css']
 })
 export class EditarPensamentoComponent implements OnInit {
+  pensamento: Pensamento ={
+    id: 0,
+    conteudo:'',
+    autoria:'',
+    modelo:''
+  }
 
-  constructor() { }
+  constructor(private service: PensamentoService,
+    private router: Router,
+    private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id')
+    this.service.buscarPoId(parseInt(id!)).subscribe((pensamento =>{
+      this.pensamento = pensamento
+    }))
+
+  }
+  editarPensamento(){
+    this.service.editar(this.pensamento).subscribe(() =>{
+      this.router.navigate(['/listarPensamento'])
+    })
+
+  }
+  cancelarPensamento(){
+    this.router.navigate(['/listarPensamento'])
+
   }
 
 }
